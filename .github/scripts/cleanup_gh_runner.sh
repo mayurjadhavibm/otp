2b50f395a6196b60a6fbf2406cfda48301b60e30
mayurjadhavibm <111068269+mayurjadhavibm@users.noreply.github.com>
2024-04-12 23:15:40 +0530

    Update cleanup_gh_runner.sh AGENT_TOOLSDIRECTORY error

--- .github/scripts/cleanup_gh_runner.sh
+++ .github/scripts/cleanup_gh_runner.sh
@@ -7,6 +7,6 @@ set -euo pipefail {0}
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 sudo rm -rf "/usr/local/share/boost"
-sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+#commented by MJ sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 docker system prune -a -f
 df -h
